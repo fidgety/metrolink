d5192a608e787c2f2f82080d86d68c6e734e20c4
Matthew Beasley <matthew.beasley@gmail.com>
2016-11-24 16:12:22 +0000

    all the :tram: stops

--- app/stopsList/grab.js
+++ app/stopsList/grab.js
@@ -3,16 +3,25 @@ const dedupe = require('dedupe');
 const fs = require('fs');
 
 const letters = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-const flatten = results => [].concat.apply([], results);
+
+const flatten = results => [].concat(...results);
+
 const save = obj => fs.writeFileSync('./stops.json', JSON.stringify(obj, null, 4));
 
-Promise.all(letters.map(letter => request(`https://beta.tfgm.com/api/search?q=${letter}&type=tram-stop-gm`)
-        .then(body => JSON.parse(body).items)
-        .then(items => items.map(item => ({
-            id: item.id.replace('-tram', ''),
-            name: item.title
-        })))
-    ))
+const mapResults = result => result.items.map(item => ({
+    id: item.id.replace('-tram', ''),
+    name: item.title.replace(' Tram Stop', '')
+}));
+
+const getStops = letter => request({
+    url: `https://beta.tfgm.com/api/search?q=${letter}&type=tram-stop-gm`,
+    json: true
+}).then(mapResults);
+
+Promise.all(letters.map(getStops))
     .then(flatten)
     .then(dedupe)
-    .then(save);
+    .then(save)
+    .catch((e) => {
+        console.error('something went wrong, sorry', e);
+    });
