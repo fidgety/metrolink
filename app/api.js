33a09f4e9d34825644e563e65aef13c5cf083d15
Matthew Beasley <matthew.beasley@gmail.com>
2017-01-04 23:33:38 +0000

    add a touch of tracking

--- app/api.js
+++ app/api.js
@@ -1,6 +1,9 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
+const ua = require('universal-analytics');
+
+const visitor = ua(process.env.UA).debug();
 
 const app = express();
 
@@ -24,6 +27,7 @@ app.use((req, res, next) => {
 });
 
 app.get('/station/:station', (req, res) => {
+    visitor.pageview(`/${req.params.station}`).send();
     request(`http://beta.tfgm.com/public-transport/stations/${req.params.station}-tram`)
         .then(parseHtml)
         .then((json) => {
