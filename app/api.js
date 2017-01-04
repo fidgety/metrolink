d63ba32fec6822d3215c6dbf0e92723fc990a638
Matthew Beasley <matthew.beasley@gmail.com>
2017-01-04 23:57:26 +0000

    add caching into app

--- app/api.js
+++ app/api.js
@@ -1,11 +1,13 @@
 const request = require('request-promise');
 const cheerio = require('cheerio');
 const express = require('express');
+const apicache = require('apicache');
 const ua = require('universal-analytics');
 
 const visitor = ua(process.env.UA);
 
 const app = express();
+const cache = apicache.middleware;
 
 const rows = '#departure-items tr';
 const destination = '.departure-destination';
@@ -20,19 +22,26 @@ const parseHtml = (html) => {
     })).toArray();
 };
 
+const trackHit = (req, res, next) => {
+    visitor.pageview(`/${req.params.station}`).send();
+    next();
+};
+
 app.use((req, res, next) => {
     res.header('Access-Control-Allow-Origin', '*');
     res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
     next();
 });
 
-app.get('/station/:station', (req, res) => {
-    visitor.pageview(`/${req.params.station}`).send();
-    request(`http://beta.tfgm.com/public-transport/stations/${req.params.station}-tram`)
-        .then(parseHtml)
-        .then((json) => {
-            res.json(json);
-        });
-});
+app.get('/station/:station',
+    trackHit,
+    cache('10 seconds'),
+    (req, res) => {
+        request(`http://beta.tfgm.com/public-transport/stations/${req.params.station}-tram`)
+            .then(parseHtml)
+            .then((json) => {
+                res.json(json);
+            });
+    });
 
 module.exports = app;
