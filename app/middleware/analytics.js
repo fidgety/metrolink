17d764a3308db80073f532ddae894c6673855682
Matthew Beasley <matthew.beasley@gmail.com>
2017-01-22 22:12:22 +0000

    a tad more tracking

--- app/middleware/analytics.js
+++ app/middleware/analytics.js
@@ -4,8 +4,18 @@ const ua = require('universal-analytics');
 const visitor = ua(process.env.UA);
 
 const trackHit = (req, res, next) => {
-    visitor.pageview(`/${req.params.station}`).send();
+    const userAgent = req.headers['user-agent'] || '';
+    const device = userAgent.indexOf('Darwin') ? 'ios' : 'android';
+
+    visitor
+        .pageview(`/${req.params.station}`)
+        .event('device', device)
+        .send();
+
     next();
 };
 
-module.exports = trackHit;
+module.exports = (app) => {
+    app.use(ua.middleware('UA-XXXX-Y', { cookieName: '_ga' }));
+    app.use(trackHit);
+};
